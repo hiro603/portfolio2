a7bb76801858dc274152a91915808d69ac3e719c
nng88 <200077567+hiro603@users.noreply.github.com>
2026-06-13 23:17:55 +0900

    ScrollTrigger登録の明示・style.css同期・実装プランを追加

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,8 @@ import { initializeHamburgerMenu } from "./component/hamburger-menu.js";
 import { initializeHeaderBackgroundToggle } from "./component/header-background-toggle.js";
 import { initializeScrollReveal } from "./component/scroll-reveal.js";
 
+gsap.registerPlugin(ScrollTrigger);
+
 initializeHamburgerMenu();
 initializeHeaderBackgroundToggle();
 initializeScrollReveal();
