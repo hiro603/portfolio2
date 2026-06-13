c007adf6f40f86a7ed2c3c2df82113c5ff3aebca
nng88 <200077567+hiro603@users.noreply.github.com>
2026-06-13 23:14:32 +0900

    スクロールリベール演出を追加しmain.jsを整理

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 import { initializeHamburgerMenu } from "./component/hamburger-menu.js";
 import { initializeHeaderBackgroundToggle } from "./component/header-background-toggle.js";
+import { initializeScrollReveal } from "./component/scroll-reveal.js";
 
 initializeHamburgerMenu();
 initializeHeaderBackgroundToggle();
+initializeScrollReveal();
